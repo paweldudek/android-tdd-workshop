c629563ef31c5c83d9cdb09aab82bb69a9f4dbad
Przemek Jakubczyk <pjakubczyk@gmail.com>
2014-05-05 22:33:56 +0200

    Adding test for starting new activity

--- robolectric/src/main/java/pl/mobilewarsaw/robolectric/MainActivity.java
+++ robolectric/src/main/java/pl/mobilewarsaw/robolectric/MainActivity.java
@@ -1,13 +1,29 @@
 package pl.mobilewarsaw.robolectric;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    public Button activityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        activityButton = (Button) findViewById(R.id.go_to_next);
+
+        activityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }
+        });
     }
+
 }
--- robolectric/src/test/java/pl/mobilewarsaw/robolectric/MainActivityTest.java
+++ robolectric/src/test/java/pl/mobilewarsaw/robolectric/MainActivityTest.java
@@ -1,8 +1,17 @@
 package pl.mobilewarsaw.robolectric;
 
+import android.content.Intent;
+import android.widget.TextView;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowIntent;
+import org.robolectric.shadows.ShadowTextView;
+
+import static org.junit.Assert.assertEquals;
+
 
 /**
  * Created by rudy on 05.05.14.
@@ -11,8 +20,24 @@ import org.robolectric.RobolectricTestRunner;
 public class MainActivityTest {
 
     @Test
-    public void testStub(){
-        assert true;
+    public void checkIfActivityButtonHasListener() {
+        MainActivity mainActivity = Robolectric.buildActivity(MainActivity.class).create().get();
+
+        ShadowTextView shadowButton = Robolectric.shadowOf(mainActivity.activityButton);
+
+        assert shadowButton.getOnClickListener() != null;
     }
-    
+
+    @Test
+    public void checkIfActivityListenerStartsSecondActivity() {
+        MainActivity mainActivity = Robolectric.buildActivity(MainActivity.class).create().get();
+
+        mainActivity.activityButton.performClick();
+
+        ShadowActivity shadowActivity = Robolectric.shadowOf(mainActivity);
+        Intent startedIntent = shadowActivity.getNextStartedActivity();
+        ShadowIntent shadowIntent = Robolectric.shadowOf(startedIntent);
+        assertEquals(SecondActivity.class.getName(), shadowIntent.getComponent().getClassName());
+    }
+
 }
